be6339cc95f2e136a20322948c160ac93426ae74
sangwooook <48618687+sangwooook@users.noreply.github.com>
2021-05-24 15:40:05 +0900

    Update get_dataset.py

--- get_dataset.py
+++ get_dataset.py
@@ -1,31 +1,20 @@
 #get mfcc vectors from audio
 
 from google.colab import files
-# import librosa.display
-# import librosa
+import librosa.display
+import librosa
 import numpy as np
-import random
 
 files.upload()
 
-
 def make_dataset():
     path = "./testaudio.wav"  # local audio file.
-    sample_rate = 16000
-
+    sample_rate = 12600
+    win_length = sample_rate / (1000/20)
     x = librosa.load(path, sample_rate)[0]
-    S = librosa.feature.melspectrogram(x, sr=sample_rate, win_length=200, hop_length=160)
+    S = librosa.feature.melspectrogram(x, sr=sample_rate, win_length=win_length, hop_length=win_length/2)
     log_S = librosa.power_to_db(S, ref=np.max)
     mfcc = librosa.feature.mfcc(S=log_S, n_mfcc=35)
     delta2_mfcc = librosa.feature.delta(mfcc, order=2)  # audio file is saved after it is vectorized
 
-    # datas = []
-    # i = 0
-    # for each_row in delta2_mfcc:
-    #   datas.extend(each_row)
-    #   if(i < 6):
-    #     datas.append(0)
-    #   i+=1
-
-    # return datas
     return delta2_mfcc
