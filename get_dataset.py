cf6f1653745910c3174af2c4b5b65f009c3da87b
sangwooook <48618687+sangwooook@users.noreply.github.com>
2021-05-24 18:18:06 +0900

    Update get_dataset.py

--- get_dataset.py
+++ get_dataset.py
@@ -7,12 +7,13 @@ import numpy as np
 
 files.upload()
 
+
 def make_dataset():
     path = "./testaudio.wav"  # local audio file.
     sample_rate = 12600
-    win_length = sample_rate / (1000/20)
+
     x = librosa.load(path, sample_rate)[0]
-    S = librosa.feature.melspectrogram(x, sr=sample_rate, win_length=win_length, hop_length=win_length/2)
+    S = librosa.feature.melspectrogram(x, sr=sample_rate, win_length=200 , hop_length=160)
     log_S = librosa.power_to_db(S, ref=np.max)
     mfcc = librosa.feature.mfcc(S=log_S, n_mfcc=35)
     delta2_mfcc = librosa.feature.delta(mfcc, order=2)  # audio file is saved after it is vectorized
