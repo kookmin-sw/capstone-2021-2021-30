3424142b2412573daa5a737e886220deb9a77719
sangwooook <48618687+sangwooook@users.noreply.github.com>
2021-05-27 13:43:01 +0900

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -2,54 +2,61 @@ import urllib
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-
+import librosa
 import tensorflow as tf
-from tensorflow.keras.layers import Embedding , LSTM , Dense , Bidirectional , Flatten
+from tensorflow.keras.layers import Embedding , LSTM , Dense , Flatten
 from tensorflow.keras.models import Sequential , load_model
 from tensorflow.keras.callbacks import ModelCheckpoint
-
 from sklearn.preprocessing import MinMaxScaler
+from keras.preprocessing.sequence import pad_sequences
 
 scaler = MinMaxScaler()
-audio_datasets = make_dataset()
-label_tmp = make_keypoint_data()
+audio_datas = make_dataset()
+labels = make_keypoint_data()
+for i in range(26):
+  labels.pop()
+#labels = change_onehot(labels)
+# audio_datas = scaler.fit_transform(audio_datas)
 
-for i in range(234255-234186):
-  label_tmp.append(random.random())
-labels = np.array(label_tmp , dtype=np.float32)
-
-audio_datasets = np.array(audio_datasets , dtype = np.float32)
-scaler.fit(audio_datasets)
-audio_datasets = scaler.fit_transform(audio_datasets)
-audio_datasets = np.reshape(audio_datasets, (234255,1))
+#make labels datas being placed bwteen 0 ~ 29
+for i in range(len(labels)):
+  if labels[i] < 0: labels[i] +=30
+labels = np.array(labels)
 
 
-train_size = 150000
+processed_audio_datas = audio_preprocessing(audio_datas)
+processed_audio_datas = np.array(processed_audio_datas)
 
-train_datas = audio_datasets[:train_size]
-train_labels = labels[:train_size]
+audio_datas = processed_audio_datas
+scaler = MinMaxScaler()
+audio_datas = scaler.fit_transform(audio_datas)
+#padding
+audio_datas = pad_sequences(audio_datas , maxlen=30,padding='post', truncating='post')
 
-test_datas = audio_datasets[train_size:]
-test_labels = labels[train_size:]
+train_size = 200000
 
-max_len = 150000
-embedding_dim = 8 #
-keypoint_size = 1697*3
+train_datas =audio_datas[:train_size]
+train_labels= labels[:train_size]
+test_datas = audio_datas[train_size:60000]
+test_labels= labels[train_size:60000]
+
+max_len = 30
+embedding_dim =2#
+keypoint_size = 30
 model = Sequential([
     Embedding(keypoint_size,embedding_dim , input_length = max_len),
-    Bidirectional(LSTM(64,return_sequences = "True")),
-    Bidirectional(LSTM(64)),
-    Dense(1)
+    LSTM(64,return_sequences="True"),
+    Flatten(input_shape = (1,30)),
+    Dense(30,activation='softmax')
 ])
 #model.summary()
 
-model.compile(optimizer="adam" , loss="mean_squared_error" , metrics = ["accuracy"])
+#activative function -> softmax (has 30 classes) , loss function -> sparse_categorical_crossentropy ( each value of labels are integer (0~29))
+model.compile(optimizer="adam" , loss="sparse_categorical_crossentropy" , metrics = [tf.keras.metrics.SparseCategoricalAccuracy()])
+
 checkpoint_callback = ModelCheckpoint("test.h5",
                                       save_best_only = True,
                                       monitor = "val_loss",
                                       verbose = 1)
-hist = model.fit(train_datas , train_labels,
-                 validation_data = (test_datas , test_labels),
-                 epochs = 2,
-                 callbacks = [checkpoint_callback])
+hist = model.fit(train_datas , train_labels, epochs = 2)
 
